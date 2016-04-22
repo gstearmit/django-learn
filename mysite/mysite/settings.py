8ba788779c205fb9b183aca62f6a3383df5e3ace
Felipe Pontes <felipemfpontes@gmail.com>
2016-04-22 14:27:05 -0300

    Update settings.py
    Setting database for Travis CI

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'database',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
