7591e256b17a5b3560b2811bc6cde166dd174258
felipemfp <felipemfpontes@gmail.com>
2016-04-22 14:59:31 -0300

    Setting the environment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
+
+
+# Set the environment
+ENV = os.environ.get('ENV', default='Development')
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,9 +29,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
     default='e5#m*b#8+v^0$ab)3h(8f^a7q23)@)ol&-czz7j7$*2r0%rt$%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if ENV == 'Production':
+    DEBUG = False
+else:
+    DEBUG = True
 
-ALLOWED_HOSTS = ['hello-django.herokuapp.com']
+if ENV == 'Production':
+    ALLOWED_HOSTS = ['hello-django.herokuapp.com']
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -84,9 +94,11 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if ENV == 'Production':
+    # Update database configuration with $DATABASE_URL.
+    import dj_database_url
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -125,17 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-   os.path.join(PROJECT_ROOT, 'static'),
-)
+if ENV == 'Production':
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+       os.path.join(PROJECT_ROOT, 'static'),
+    )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+if ENV == 'Production':
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
